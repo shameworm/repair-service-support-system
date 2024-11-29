d421e0e78f31a21e8af7241b487d8865df6a091f
shameworm <dmytro.omelchenko.shameworm@gmail.com>
2024-11-29 15:01:57 +0200

    refactor(backend/technicia.schema): add password and isAdmin fields in db schema

--- backend/src/models/technician.schema.ts
+++ backend/src/models/technician.schema.ts
@@ -4,12 +4,16 @@ export interface ITechnician extends Document {
   name: string;
   specialization: string;
   contactInfo: string;
+  password: string;
+  isAdmin: boolean;
 }
 
 const technicianSchema = new Schema<ITechnician>({
   name: { type: String, required: true },
   specialization: { type: String, required: true },
-  contactInfo: { type: String, required: true }
+  contactInfo: { type: String, required: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: false }
 });
 
 export const Technician = mongoose.model<ITechnician>(
